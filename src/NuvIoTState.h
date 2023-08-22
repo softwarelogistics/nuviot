ca086ce2323cc22a3d8dc560e30c6fb5fc72f4e6
Kevin D. Wolf <kevinw@software-logistics.com>
2023-08-21 21:22:53 -0400

    bump revision to 3.8

--- src/NuvIoTState.h
+++ src/NuvIoTState.h
@@ -152,7 +152,7 @@ public:
     String getFirmwareSKU();
     String getDeviceModelKey();
 
-    String getLibraryVersion() { return "3.6"; }
+    String getLibraryVersion() { return "3.8"; }
 
     
     WiFiConnectionStates getWiFiState() { return m_isWiFiConnectionState; }
